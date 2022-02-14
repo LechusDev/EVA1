38e13bb699743f659624e9601a9f8a287b6f3493
LechusDev <99498556+LechusDev@users.noreply.github.com>
2022-02-14 09:43:06 -0800

    Add files via upload

---
+++ EVA1_1_HOLAMUNDO/src/main/java/EVA1_1_HOLAMUNDO.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+/**
+ * PALABRAS EN AZUL: palabras reservadas: uso exclusivo del lenguaje
+ * PALABRAS EN NEGRO: IDENTIFICADORES --  variables, constantes, clases, funciones, objetos, etc
+ * []: arreglo
+ * {}: delimitadores - bloques de código
+ * ctrl+espacio
+ * shift + flechita abajo para copiar
+ */
+/**
+ *
+ * @author James
+ */
+public class EVA1_1_HOLAMUNDO {//CUERPO DE LA CLASE
+    int x;//IDENTIFICADOR
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {//FUNCION
+        // TODO code application logic here
+        System.out.println("Hello World!!");//INSTRUCCION
+        for (int i = 0; i < 10; i++){//INSTRUCCION
+            
+            
+        }
+    }
+    
+}
+ //CLASS, INTERFACE, ENUM
+//Todas las instrucciones deben llevan ;
+//args Es un arreglo
+//Public class es el cuerpo de la clase y el archivo se debe llamar igual
+//Main es el inicio 
+//Java es un lenguaje de dos estapas 
