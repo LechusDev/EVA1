85c093a7f887494dbc127e252a4668ef58b3741a
LechusDev <99498556+LechusDev@users.noreply.github.com>
2022-03-03 08:59:52 -0800

    Add files via upload

---
+++ JavaApplication69/src/main/java/JavaApplication69.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Lechus
+ */
+public class JavaApplication69 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int val1=100;
+        System.out.println("Val1 = " + val1);
+        int copia = val1;
+        System.out.println("copia = " + copia);
+        //MODIFICAR COPIA
+        copia = 99999;
+        System.out.println("val1 = " + val1);
+        System.out.println("copia = " + copia);
+        
+    }
+    
+}
